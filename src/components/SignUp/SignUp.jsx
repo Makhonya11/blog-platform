39ab7b858e47c4c81343ebb8ac2a89bda50481ac
Makhach <mmaaamm666@gmail.com>
2025-04-30 08:26:40 +0300

    add vsl

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,11 +1,17 @@
 import styles from './SignUp.module.scss'
 import { useNavigate } from 'react-router'
 import { useDispatch } from 'react-redux'
+import { useForm } from 'react-hook-form'
 import { registerUser } from '../../store/UserSlice'
 
 const SignUp = () => {
   const navigate = useNavigate()
   const dispatch = useDispatch()
+  const {
+register,
+handleSubmit,
+formState:{errors},
+  } = useForm()
   const getFormData = (e) => {
     const formData = new FormData(e.target)
     return {
@@ -20,7 +26,7 @@ const SignUp = () => {
   return (
     <div className={styles.signUp}>
       <h3>Create new account</h3>
-      <form action="/submit" method="post"
+      <form 
       onSubmit={(e) => {
         e.preventDefault()
         dispatch(registerUser(getFormData(e)))
@@ -29,15 +35,62 @@ const SignUp = () => {
       }}
       >
         <label htmlFor="username">Username</label>
-        <input type="text" id="username" name="username" placeholder="Username" />
+        <input type="text" id="username" name="username" placeholder="Username" 
+        {...register("username", {
+          required: "Обязательное поле",
+        minLength: {
+          value:3,
+          message: 'От 3 до 20 символов',
+                },
+          maxLength:{
+            value:20,
+            message: 'От 3 до 20 символов',
+          },
+          pattern:{
+            value:/^[a-z0-9]+$/,
+            message: 'Только латинские буквы в нижнем регистре и цифры'
+          }
+        })}
+        />
+        {errors.username && <p>{errors.username.message}</p>}
         <label htmlFor="email">Email address</label>
-        <input type="email" id="email" name="email" placeholder="Email address" />
+        <input type="email" id="email" name="email" placeholder="Email address" 
+         {...register("email", {
+          required: "Обязательное поле",
+          pattern:{
+            value:/^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+            message: 'Проверьте правильность электронной почты'
+          }
+        })}
+        />
+        {errors.email && <p>{errors.email.message}</p>}
         <label htmlFor="password">Password</label>
-        <input type="password" id="password" name="password" placeholder="Password" />
+        <input type="password" id="password" name="password" placeholder="Password" 
+        {...register("password", {
+          required: "Обязательное поле",
+        minLength: {
+          value:6,
+          message: 'От 6 до 40 символов',
+                },
+          maxLength:{
+            value:40,
+            message: 'От 6 до 40 символов',
+          },
+          pattern:{
+            value:/^\S+$/,
+            message: 'Пробелы недопустимы'
+          }
+        })}
+        />
+        {errors.password && <p>{errors.password.message}</p>}
         <label htmlFor="confirm">Repeat Password</label>
         <input type="password" id="confirm" placeholder="Password" />
         <div className={styles.agree}>
-          <input type="checkbox" id="agree" />
+          <input type="checkbox" id="agree" 
+          {...register("agree", {
+            required: "Примите соглашение",
+          })}
+          />
           <label htmlFor="agree">I agree to the processing of my personal information</label>
         </div>
         <button type="submit">Create</button>
